2e5cbe20bb42a5cae561cff931ca1a80ed812557
Tim Bang <racer4308@gmail.com>
2021-11-25 10:39:35 +0100

    Add VW7 Creature

--- src/main/VW7/Kreaturen/Creature.java
+++ src/main/VW7/Kreaturen/Creature.java
@@ -1,2 +1,55 @@
-package main.VW7.Kreaturen;public class Creature {
+package main.VW7.Kreaturen;
+
+import java.util.Random;
+
+public class Creature {
+    String name;
+    int vitality;
+    boolean lovely;
+    Creature[] parentOf = new Creature[5];
+    int counter = 0;
+
+    static Random rand = new Random();
+
+    Creature(String name) {
+        this(name, rand.nextBoolean(), rand.nextInt(101));
+    }
+
+    Creature(String name, boolean lovely, int vitality) {
+        assert vitality >= 0 && vitality <= 100 : "0 <= Vitalität <= 100";
+        assert name != null && !name.isEmpty() : "Gib mir einen richtigen Namen";
+        this.name = name;
+        this.lovely = lovely;
+        this.vitality = vitality;
+    }
+
+    void reduceVitality(int amount) {
+        assert amount >= 0;
+        vitality -= amount; // vitality = vitality - amount;
+        if (vitality < 0) vitality = 0;
+    }
+
+    boolean canJoin(Creature other) {
+        return this != other && this.vitality >= 50 && other.vitality >= 50 && counter < parentOf.length;
+    }
+
+    Creature join(Creature other) {
+        assert canJoin(other) : "Unable to join " + other;
+        boolean loveliness = this.lovely == other.lovely ? this.lovely : rand.nextBoolean();
+        Creature child = new Creature(this.name + other.name, loveliness, (this.vitality + other.vitality) / 2);
+        parentOf[counter++] = child;
+        reduceVitality(33);
+        return child;
+    }
+
+    public String toString() {
+        String returnString = name + "(" + (lovely ? "lovely" : "unlovely") + ", " + vitality + ")";
+        for (Creature c: parentOf) {
+            if (c == null)
+                continue;
+            returnString += "\nparentof " + c;
+        }
+        return returnString;
+
+    }
 }
--- src/main/VW7/Kreaturen/CreatureTest.java
+++ src/main/VW7/Kreaturen/CreatureTest.java
@@ -1,4 +1,23 @@
+package main.VW7.Kreaturen;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CreatureTest {
-  
+
+    // TODO Make better tests when needed. For now just for execution.
+    @Test
+    public void TestCreature() {
+        Creature john = new Creature("John", true, 100);
+        Creature vanessa = new Creature("Vanessa", true, 100);
+        System.out.println(john);
+        System.out.println(vanessa);
+        System.out.println(john.canJoin(vanessa));
+        Creature baby = john.join(vanessa);
+        baby.vitality = 100;
+        baby.join(vanessa);
+
+        System.out.println(john);
+    }
 }
